e887c4fc91c2552c11200958ddc8e1cd674cee11
Alfaa719 <nurul.hidayat.f3@gmail.com>
2021-09-07 20:30:12 +0700

    first-commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public PlayerController player1, player2;
+    public BallController ball;
+    private Rigidbody2D player1Rb, player2Rb, ballRb;
+    private CircleCollider2D ballCollide;
+    public Trajectory trajectory;
+
+    private bool isDebugWindowShown = false;
+
+    public int maxScore;
+
+    void Start()
+    {
+        player1Rb = player1.GetComponent<Rigidbody2D>();
+        player2Rb = player2.GetComponent<Rigidbody2D>();
+        ballRb = ball.GetComponent<Rigidbody2D>();
+        ballCollide = ball.GetComponent<CircleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnGUI()
+    {
+        GUI.Label (new Rect (Screen.width / 2 - 150 - 12, 20, 100, 100), " " + player1.Score);
+        GUI.Label (new Rect (Screen.width / 2 + 150 + 12, 20, 100, 100), " " + player2.Score);
+
+        if (GUI.Button(new Rect (Screen.width / 2 - 60, 35, 120, 53) , "Restart"))
+        {
+            player1.ResetScore();
+            player2.ResetScore();
+
+            ball.SendMessage("RestartGame", 0.5f, SendMessageOptions.RequireReceiver);
+        }
+        if(player1.Score == maxScore)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 150, Screen.height / 2 - 10, 2000, 1000), "Player 1 Win!!");
+            ball.SendMessage("ResetBall", null, SendMessageOptions.RequireReceiver);
+        }
+        else if (player2.Score == maxScore)
+        {
+            GUI.Label(new Rect(Screen.width / + 30, Screen.height / 2 - 10, 2000, 1000), "Player 2 Win!!");
+            ball.SendMessage("ResetBall", null, SendMessageOptions.RequireReceiver);
+        }
+
+        if (isDebugWindowShown)
+        {
+            Color oldColor = GUI.backgroundColor;
+            GUI.backgroundColor = Color.red;
+            // Simpan variabel-variabel fisika yang akan ditampilkan. 
+            float ballMass = ballRb.mass;
+            Vector2 ballVelocity = ballRb.velocity;
+            float ballSpeed = ballRb.velocity.magnitude;
+            Vector2 ballMomentum = (ballMass * ballVelocity);
+            float ballFriction = ballCollide.friction;
+
+            float impulsePlayer1X = player1.LastContactPoint.normalImpulse;
+            float impulsePlayer1Y = player1.LastContactPoint.tangentImpulse;
+            float impulsePlayer2X = player2.LastContactPoint.normalImpulse;
+            float impulsePlayer2Y = player2.LastContactPoint.tangentImpulse;
+
+            // Tentukan debug text-nya
+            string debugText =
+                "Ball mass = " + ballMass + "\n" +
+                "Ball velocity = " + ballVelocity + "\n" +
+                "Ball speed = " + ballSpeed + "\n" +
+                "Ball momentum = " + ballMomentum + "\n" +
+                "Ball friction = " + ballFriction + "\n" +
+                "Last impulse from player 1 = (" + impulsePlayer1X + ", " + impulsePlayer1Y + ")\n" +
+                "Last impulse from player 2 = (" + impulsePlayer2X + ", " + impulsePlayer2Y + ")\n";
+            GUIStyle guiStyle = new GUIStyle(GUI.skin.textArea);
+            guiStyle.alignment = TextAnchor.UpperCenter;
+            GUI.TextArea(new Rect(Screen.width / 2 - 200, Screen.height - 200, 400, 110), debugText, guiStyle);
+            GUI.backgroundColor = oldColor;
+            trajectory.enabled = !trajectory.enabled;
+        }
+        // Toggle nilai debug window ketika pemain mengeklik tombol ini.
+        if (GUI.Button(new Rect(Screen.width / 2 - 60, Screen.height - 73, 120, 53), "TOGGLE\nDEBUG INFO"))
+        {
+            isDebugWindowShown = !isDebugWindowShown;
+        }
+    }
+}
---
+++ Assets/SideWall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideWall : MonoBehaviour
+{
+    public PlayerController player;
+
+    [SerializeField] private GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Ball")
+        {
+            player.IncremetScore();
+            if (player.Score < gameManager.maxScore)
+            {
+                collision.gameObject.SendMessage("RestartGame", 2f, SendMessageOptions.RequireReceiver);
+            }
+        }
+    }
+}
